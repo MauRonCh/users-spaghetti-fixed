b1351103950ea11495e92b960d096fe0a8bacf9f
Mauricio Ronceros <mronceroschavez@gmail.com>
2022-11-28 16:08:55 -0500

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,13 +1,34 @@
-const express = require('express')
+const express = require('express');
+const db = require('./utils/database');
+const userRouter = require('./users/user.router');
 
-const port = 9000
+const port = 9000;
+const app = express();
 
-const app = express
+db.authenticate()
+    .then(() => {
+        console.log('Database aunthenticated!');
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+db.sync()
+    .then(() => {
+        console.log('Database Synced!');
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Ok!'})
 }) 
 
-app.listen(() => {
+app.use('/api/v1', userRouter);
+
+app.listen(port, () => {
     console.log(`Server started at port ${port}`)
 })
--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,4 +1,44 @@
+//* {
+//*     id: 1,
+//*     first_name: 'string',
+//*     last_name: 'string',
+//*     email: 'string',
+//*     password: 'string',
+//*     birthday: 'YYYY/MM/DD'
+//* }
 
+const { DataTypes } = require('sequelize');
 
+const db = require('../utils/database');
 
+const Users = db.define('users', {
+    id: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        autoIncrement: true
+    },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    birthday: {
+        type: DataTypes.DATEONLY
+    }
+}, {
+    timestamps: false
+})
 
+module.exports = Users;
